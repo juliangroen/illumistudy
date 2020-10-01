1e442e5738349e5935dca702f82f795eb1919cf5
Julian <julian@jgroen.com>
2020-10-01 14:52:02 -0600

    included some transitions and animations

--- src/components/CardHolder.svelte
+++ src/components/CardHolder.svelte
@@ -1,6 +1,7 @@
 <script>
     import { createEventDispatcher } from 'svelte';
     import { flip } from 'svelte/animate';
+    import { fade } from 'svelte/transition';
     const dispatch = createEventDispatcher();
     import StudyCard from './StudyCard.svelte';
 
@@ -8,9 +9,11 @@
 </script>
 
 {#each allCards as { id, question, answer } (id)}
-    <div animate:flip={{ duration: 500 }}>
+    <div out:fade={{ duration: 100 }} animate:flip={{ duration: 500 }}>
         <StudyCard on:delCard={dispatch('handleDelCard', id)}>
             <span slot="question">{question}</span><span slot="answer">{answer}</span>
         </StudyCard>
     </div>
+{:else}
+    <div class="text-center">Nothing to see here... <span role="img" aria-label="alien">👽</span></div>
 {/each}
--- src/components/StudyCard.svelte
+++ src/components/StudyCard.svelte
@@ -1,7 +1,13 @@
 <script>
     import { createEventDispatcher } from 'svelte';
+    import { fade, slide } from 'svelte/transition';
     const dispatch = createEventDispatcher();
     let cardFront = true;
+    let visible = true;
+    const handleDelete = () => {
+        visible = false;
+        dispatch('delCard');
+    };
 </script>
 
 <style lang="postcss">
@@ -14,16 +20,20 @@
 </style>
 
 <div
-    class:flipped={cardFront === false}
+    class:flipped={cardFront === false && visible === true}
     class="relative bg-gray-400 border-4 border-gray-400 hover:border-gray-500 cursor-pointer text-center h-64 p-8 mb-4 break-words"
     on:click={() => (cardFront = !cardFront)}>
     <span
         class="absolute right-0 top-0 bg-gray-500 hover:bg-red-400 hover:text-red-800 text-lg font-bold leading-none opacity-50 hover:opacity-100 py-1 px-2 m-2"
-        on:click|self={() => dispatch('delCard')}>x</span>
+        on:click|self={handleDelete}>x</span>
     {#if cardFront}
-        <slot name="question">No Question</slot>
+        <div in:fade|local={{ duration: 200 }}>
+            <slot name="question">No Question</slot>
+        </div>
     {:else}
-        <slot name="answer">No Answer</slot>
+        <div in:fade|local={{ duration: 200 }}>
+            <slot name="answer">No Answer</slot>
+        </div>
     {/if}
     <div class="absolute right-0 bottom-0 text-indigo-600 px-4 py-2">
         Click To See
